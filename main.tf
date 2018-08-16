35824e963c2dea7c4913cbda407509b3f85f2189
Conner Swann <me@connerswann.me>
2018-08-16 00:54:30 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ terraform {
   backend "s3" {}
 }
 
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
 locals {
   max_subnet_length = "${max(length(var.private_subnets), length(var.elasticache_subnets), length(var.database_subnets), length(var.redshift_subnets))}"
   nat_gateway_count = "${var.single_nat_gateway ? 1 : (var.one_nat_gateway_per_az ? length(var.azs) : local.max_subnet_length)}"
